c5cddcf525a62d7381157e82938538a3561ce677
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-12-22 08:26:44 +0700

    Create Test01.sql

---
+++ Test01.sql
@@ -0,0 +1,13 @@
+﻿create database Test01
+create table Student (
+SinhVien varchar(40),
+NgaySinh date,
+Lop varchar(40)
+)
+GO
+
+insert into Student(SinhVien, NgaySinh, Lop) values ('Nguyễn Bá Khánh','2003-11-08', 'T2109M')
+delete from Student where SinhVien = 'Vũ Viết Quý'
+update Student set SinhVien = 'Vũ Viết Quý' where SinhVien = 'Nguyễn Bá Khánh'
+
+select * from Student
